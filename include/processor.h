f87f66f1ddedbb74cfb8cb27ef1e056ecf144b32
= <ahmed.yehia.1177@gmail.com>
2020-04-23 11:17:42 +0200

    Added CPU Util

--- include/processor.h
+++ include/processor.h
@@ -2,11 +2,18 @@
 #define PROCESSOR_H
 
 class Processor {
+ private:
+  struct ProcessorState {
+    float Idle, IOWait, User, Nice, System, Irq, SoftIrq, Steal, Guest,
+        GuestNice;
+  };
+
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+  ProcessorState previousProcessorState;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,45 @@
-#include "processor.h"
+#include <fstream>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include "linux_parser.h"
+#include "processor.h"
+// DONE: Return the aggregate CPU utilization
+float Processor::Utilization() {
+  ProcessorState state;
+  std::ifstream procFile(LinuxParser::kProcDirectory +
+                         LinuxParser::kStatFilename);
+  if (procFile.is_open()) {
+    std::string line, k, v;
+    while (getline(procFile, line)) {
+      std::stringstream cpuValues(line);
+      cpuValues >> k;
+      if (k == "cpu") {
+        cpuValues >> state.User >> state.Nice >> state.System >> state.Idle
+        >> state.IOWait >> state.Irq >> state.SoftIrq >> state.Steal >>
+        state.Guest >> state.GuestNice;
+        cpuValues.str();
+        break;
+      }
+    }
+
+    // Idle
+    float prevIdle =
+        previousProcessorState.Idle + previousProcessorState.IOWait;
+    float idle = state.Idle + state.IOWait;
+    float idleDelta = idle - prevIdle;
+    // NonIdle
+    auto& pr = previousProcessorState;
+    float prevNonIdle =
+        pr.User + pr.Nice + pr.System + pr.Irq + pr.SoftIrq + pr.Steal;
+    float nonIdle = state.User + state.Nice + state.System + state.Irq +
+                    state.SoftIrq + state.Steal;
+    // Total
+    float prevTotal = prevIdle + prevNonIdle;
+    float total = idle + nonIdle;
+    float totalDelta = total - prevTotal;
+
+    previousProcessorState = state;
+    return (totalDelta - idleDelta) / totalDelta;
+  }
+
+  return 0.0;
+}
